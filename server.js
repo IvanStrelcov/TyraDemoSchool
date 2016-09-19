92ffd31a92ad3fcf38e3d2c39180df4033ba4bea
strelcov <ivan.strelcov.1986@gmail.com>
2016-09-19 14:02:35 +0300

    fix server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
-app.use(express.static('../dist'));
+// app.use(express.static('../dist'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// app.use('/', express.static('../dist'));
+app.use('/api', express.static('../dist'));
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
