8b07180a56305a20f083b98c6cc1eab1ac886b4b
strelcov <ivan.strelcov.1986@gmail.com>
2016-09-19 13:24:02 +0300

    add server.js and change package.json config for using on Heroku

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(cors());
+app.use(express.static('../dist'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', express.static('../dist'));
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.json('error', {
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(port, function () {
+  console.log('App is running on http://localhost:' + port);
+});
