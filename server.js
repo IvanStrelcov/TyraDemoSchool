fafa3b8bd3c2f186c2d5aad2d260d3e55e255d16
strelcov <ivan.strelcov.1986@gmail.com>
2016-09-19 13:46:21 +0300

    fix server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('../dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', express.static('../dist'));
+// app.use('/', express.static('../dist'));
 
 app.use((req, res, next) => {
   const err = new Error('Not Found');
